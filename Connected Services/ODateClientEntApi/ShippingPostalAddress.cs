2295b8f09436ab31b4cce19f02412b1ea94ec51a
Wolter Kaper <w.h.kaper@uva.nl>
2021-05-02 14:49:49 +0200

    ShippingPostalAddress + 2 calculated properties
    Calculated properties recalculated on the client side

---
+++ Connected Services/ODateClientEntApi/ShippingPostalAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entocare.NAV
+{
+    /// <summary>
+    /// Two calculated properties from TabExt.70002.MySalesHeader.AL are repeated here, for efficiency sake
+    /// there seems to be no efficient way to calculate them before the transfer in AL
+    /// </summary>
+    public partial class ShippingPostalAddress
+    {
+        //transferred to the generated class! Zie de README...!
+    }
+}
