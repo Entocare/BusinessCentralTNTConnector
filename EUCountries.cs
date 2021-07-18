edabc2bf26f6f8bf5dd7e34cf778a2aef5f9e426
Wolter Kaper <w.h.kaper@uva.nl>
2021-07-18 10:45:37 +0200

    Get GB finally out the EU

--- EUCountries.cs
+++ EUCountries.cs
@@ -15,7 +15,7 @@ namespace BusinessCentralTNTConnector
         /// Set of Counties in the EU
         /// </summary>
         public static readonly HashSet<string> EUCountries0 = new HashSet<string>()
-            { "AT", "BE", "DE", "DK", "ES", "FI", "FR", "GB", "IT", "LU", "NL", "PL", "PT", "SE", "SK"};
+            { "AT", "BE", "DE", "DK", "ES", "FI", "FR", "IT", "LU", "NL", "PL", "PT", "SE", "SK"};
 
         /// <summary>
         /// Check whether a given 2 letter CountryCode is in the EU
